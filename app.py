589beef822b709705cf901356f4e1b093bbd725f
parthp23 <149885009+parthp23@users.noreply.github.com>
2025-04-23 22:35:25 +0530

    added the main function of port 5000 app.py
    camera not opening

--- app.py
+++ app.py
@@ -235,4 +235,4 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-   app.run()
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
